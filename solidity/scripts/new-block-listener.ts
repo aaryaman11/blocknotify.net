1ecbc228fe3e5d00f517dd4293c3fed9a30dcd08
jacobmakarsky <jacobmakarsky@gmail.com>
2023-02-28 11:49:23 -0700

    adding eventListener.py

--- solidity/scripts/new-block-listener.ts
+++ solidity/scripts/new-block-listener.ts
@@ -1,49 +1,22 @@
-const { ethers } = require("ethers");
-require("dotenv").config();
+import { Alchemy, AlchemyEventType, Network } from "alchemy-sdk";
+import { ethers } from "ethers";
 
-// const provider = ethers.provider;
+const toOrFromAddr = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
-const fromOrToAddress = "0x0000000000000000000000000000000000000000";
+const settings = {
+  apiKey: "6Q8Kt1lWN5qOYC2Q_znUQuSo1r7NqMvo", // Replace with your Alchemy API Key.
+  network: Network.ETH_MAINNET, // Replace with your network.
+};
+const alchemy = new Alchemy(settings);
 
-async function main() {
-  // const provider = ethers.provider;
-  const provider = new ethers.providers.WebSocketProvider(
-    process.env.MAINNET_RPC ?? ""
-  );
+const transferEvents = {
+  address: toOrFromAddr,
+  // topics: [],
+  topics: [ethers.utils.id("Transfer(address,address,uint256)")],
+};
 
-  // // listen to pending transactions in the mempool from the node we are connected to
-  // const completion = (tx) => {
-  //   /* do some stuff */
-  //   console.log("Finished");
-  // };
-
-  // check the tx to see if it is from or to the address we are looking for
-  const checkTxHash = async (txHash) => {
-    console.log("txHash", txHash);
-  };
-
-  provider.on("alchemy_pendingTransactions", checkTxHash, {
-    toAddress: [fromOrToAddress, fromOrToAddress],
-    hashesOnly: true,
-  });
-
-  provider._websocket.on("error", async () => {
-    console.log(
-      `Unable to connect to ${process.env.MAINNET_RPC} retrying in 3s...`
-    );
-    setTimeout(main, 3000);
-  });
-
-  provider._websocket.on("close", async (code) => {
-    console.log(
-      `Connection lost with code ${code}! Attempting reconnect in 3s...`
-    );
-    provider._websocket.terminate();
-    setTimeout(main, 3000);
-  });
-}
-
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+alchemy.ws.on(transferEvents, (log, event) => {
+  console.log("New Log Detected!");
+  console.log("Block Number:", log.blockNumber);
+  console.log("Transaction Hash:", log.transactionHash);
 });
