1e52a74ef9eea0e3240a2a99321b58b11f2993b1
Aaryaman <aaryaman.sh@gmail.com>
2023-03-04 21:48:56 -0700

    Testing

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -82,12 +82,13 @@ function SignForm(props) {
     //     phoneInput.destroy();
     //     };
     // }, [])
+    // onSubmit={sign}
     
 
     if (chainState.connected) {
         return (<div className="row align-middle">
             <div className="input-group w-100 mb-1">
-                <Form onSubmit={sign}>
+                <Form>
                     <span className="input-group-text" id="basic-addon1">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -101,7 +102,7 @@ function SignForm(props) {
                                 d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"></path>
                         </svg>
                     </span>
-                    <input //have to check the ref
+                    <Input //have to check the ref
                         type="tel"
                         ref={phoneInputRef}
                         id = "phone"
@@ -110,7 +111,7 @@ function SignForm(props) {
                         aria-label="Phone Number"
                         aria-describedby="basic-addon1"
                         value={FormData.phone}
-                        invalid={!validPhoneNumber.toString()}
+                        invalid={!validPhoneNumber}
                     
                         onChange={(e) => setPhone(e.target.value)}
                     />
@@ -118,7 +119,7 @@ function SignForm(props) {
                      <Button
                         type = "submit"
                         className="btn btn-primary"
-                        // onClick={sign}
+                        onClick={sign}
                     >Sign</Button>
                    
                     
